5694547a7d90614fdb92e5a61e44594828682231
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-08-24 23:33:22 -0300

    models ready

--- api/src/models/Episode.js
+++ api/src/models/Episode.js
@@ -4,6 +4,14 @@ const { DataTypes } = require('sequelize')
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('episode', {
-
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   })
 }
