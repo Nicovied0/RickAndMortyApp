19530c29d45cac2af5f351c10fb1d61fc27de70f
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-08-25 01:02:24 -0300

    get api data ready

--- api/src/controllers/character.js
+++ api/src/controllers/character.js
@@ -7,11 +7,22 @@ const getApiCharacters = async () => {
     const allCharacter = []
     let apiUrl = 'https://rickandmortyapi.com/api/character'
 
-    for(let i = 0; i <= 1; i++){
+    for (let i = 0; i <= 1; i++) {
       let apiData = await axios(apiUrl)
-      
-    }
 
+      apiData.data.results?.forEach(e => {
+        return allCharacter.push({
+          id: e.id,
+          name: e.name,
+          species: e.species,
+          origin: e.origin.name,
+          image: e.image,
+          characterApi: true,
+        })
+      })
+      apiUrl = apiData.data.info.next
+    }
+    return allCharacter
   } catch {
     console.log('Error get api')
   }
