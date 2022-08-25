a72faaa6e3987d16064fbceb9d8a7f5525382b8f
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-08-25 01:23:13 -0300

    route get caracter ready

--- api/src/routes/character.js
+++ api/src/routes/character.js
@@ -1,1 +1,13 @@
-const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+const {Character, Episode} = require("../db");
+const {getAllInfo} = require('../controllers/character')
+
+router.get('/',async(req,res) =>{
+  const allCharacters = await getAllInfo()
+  allCharacters?
+  res.status(200).send(allCharacters):
+  res.status(404).send('Error en get /character')
+})
+
+module.exports = router
