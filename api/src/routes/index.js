e7cc581c7ff1f83525b624d29f7cc29b817af5e9
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-08-24 23:53:06 -0300

    rouse index ready

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
+const characterRoute = require('./character');
+const episodeRoute = require('./episode')
 
 const router = Router();
 
 // Configurar los routers
+router.use('/character', characterRoute)
+router.use('/episode',episodeRoute)
 
 module.exports = router;
