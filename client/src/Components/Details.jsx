6facfce8a1da9ed19f2abcf375d925b0321dbb1c
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-24 22:50:13 -0300

    details with props

--- client/src/Components/Details.jsx
+++ client/src/Components/Details.jsx
@@ -4,40 +4,42 @@ import { getDetails } from "../redux/actions/actions";
 // import { useParams } from "react-router-dom";
 
 function Details(props) {
-  const [loading, setLoading] = useState(false);  
-  const details  = useSelector((i) => i.details );
+  const [loading, setLoading] = useState(false);
+  const details = useSelector((i) => i.details);
   const dispatch = useDispatch();
-  const { id } =  props.match.params;
+  const { id } = props.match.params;
 
   useEffect(() => {
-    dispatch(getDetails(id))
-    setLoading(true)
+    dispatch(getDetails(id));
+    setLoading(true);
   }, [dispatch, id]);
 
-// console.log(details,'detalles')
-// console.log(loading,'cargando')
+  // console.log(details,'detalles')
+  // console.log(loading,'cargando')
 
-  
   return (
     <div className="detail">
-      {loading?
+      {loading ? (
         <div>
-        <h1>{details?.name}</h1>
-        <img src={details?.image} alt={details?.name} />
-
-        <h2>Episodes:</h2>
-        <ul>
-          {console.log(details)}
-          {details?.episode.map(e => (
+          <h1>{details?.name}</h1>
+          <img src={details?.image} alt={details?.name} />
+          <h2>{details?.name}</h2>
+          <h2>Origin: {details?.origin}</h2>
+          <h2>species: {details?.species}</h2>
+          <h2>Episodes:</h2>
+          <h2>species: {details?.episode}</h2>
+          <ul>
+            {console.log(details)}
+            {/* {details?.episode.map(e => (
             <li>
               Episode: {e.id} : {e.name}
             </li>
-          ))}
-        </ul>
-      </div>:
-      <div>cargando</div>
-      }
-      
+          ))} */}
+          </ul>
+        </div>
+      ) : (
+        <div>cargando</div>
+      )}
     </div>
   );
 }
